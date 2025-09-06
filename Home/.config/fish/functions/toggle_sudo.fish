84aa9f85df8777a8b7386594a2e7770caf62d62d
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-06 03:58:39 +0000

    Update toggle_sudo.fish

--- Home/.config/fish/functions/toggle_sudo.fish
+++ Home/.config/fish/functions/toggle_sudo.fish
@@ -1,13 +1,27 @@
 function toggle_sudo
-  if test (commandline) = ""
-    commandline (history --max=1 | string trim)
-  end
-  if string match -q "sudo *" -- (commandline)
-    # Remove sudo
-    set cmd (string replace -r '^sudo\s+' '' -- (commandline))
-    commandline -- $cmd
-  else
-    # Prepend sudo
-    commandline -- sudo (commandline)
-  end
+    set -l prefix 'sudo-rs '
+    set -l buf (commandline)
+    set -l pos (commandline -C)
+    if test -z "$buf"
+        set buf $history[1]
+        set pos (string length -- "$buf")
+    end
+    set -l ws (string match -r '^\s*' -- $buf)
+    set -l ws_len (string length -- "$ws")
+    set -l prefix_len (string length -- "$prefix")
+    set -l rest (string replace -r '^\s*' '' -- $buf)
+    if string match -r "^$prefix" -- "$rest"
+        set rest (string replace -r "^$prefix" '' -- $rest)
+        if test $pos -gt $ws_len
+            set pos (math "$pos - $prefix_len")
+            if test $pos -lt $ws_len; set pos $ws_len; end
+        end
+    else
+        set rest "$prefix$rest"
+        if test $pos -ge $ws_len
+            set pos (math "$pos + $prefix_len")
+        end
+    end
+    commandline -r -- "$ws$rest"
+    commandline -C -- $pos
 end
