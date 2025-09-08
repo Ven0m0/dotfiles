6b413c9cde55afdad15c3911daf8f184577d5d46
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-08 19:53:41 +0200

    Update config.fish

--- Home/.config/fish/config.fish
+++ Home/.config/fish/config.fish
@@ -107,7 +107,7 @@ abbr -a ip ip --color=auto
 abbr -a du du -hcsx
 
 # ─── Path Deduplication ─────────────────────────────────────────────────────────
-set PATH (printf "%s" "$PATH" | awk -v RS=':' '!a[$1]++ { if (NR > 1) printf RS; printf $1 }')
+set PATH (printf "%s" "$PATH" | awk -O -v RS=':' '!a[$1]++ { if (NR > 1) printf RS; printf $1 }')
 # Deduplicate PATH (preserve order) to prevent PATH bloat across reloads
 #set -l seen
 #set -l newpath
