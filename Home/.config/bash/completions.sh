00db3cac645b6a8f4243186269574a560bd27dac
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-08 20:04:24 +0200

    Update completions.sh

--- Home/.config/bash/completions.sh
+++ Home/.config/bash/completions.sh
@@ -3,29 +3,24 @@
 # ╰─────────────────╯
 # https://github.com/CodesOfRishi/dotfiles
 # Tab comletion for $EDITOR
-editor_completion() {
-	# To redraw line after fzf closes (printf '\e[5n') 
-	# This is useful when the terminal is altered by FZF, and the command line gets visually corrupted or misaligned
-	bind '"\e[0n": redraw-current-line' 2> /dev/null
-	local selected_result 
+editor_completion(){
+# To redraw line after fzf closes (printf '\e[5n') 
+# This is useful when the terminal is altered by FZF, and the command line gets visually corrupted or misaligned
+  bind '"\e[0n": redraw-current-line' 2>/dev/null
+	local selected_result
 	if selected_result="$(compgen -f -- "${COMP_WORDS[COMP_CWORD]}" | command fzf \
 		--prompt='❯ ' \
 		--height=~100% \
 		--tiebreak=begin,index \
-		--select-1 \
-		--exit-0 \
-		--exact \
+		-1 -0 -e \
 		--layout=reverse \
 		--bind=tab:down,btab:up \
 		--cycle)"; then
-
 		[[ -d "${selected_result}" ]] && selected_result="${selected_result}/" || selected_result="${selected_result} "
 		COMPREPLY=( "${selected_result}" )
 	fi
 	printf '\e[5n'
 }
-
 complete -o nospace -F editor_completion "$EDITOR"
-complete -o nospace -F editor_completion '$EDITOR'
 complete -o nospace -F editor_completion nano
-complete -o nospace -F editor_completion edit
+complete -o nospace -F editor_completion micro
