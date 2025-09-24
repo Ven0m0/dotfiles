18bc29c267b96a2d9f64aba8356c1f503bcaf93b
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-24 11:15:56 +0200

    Update vx.fish

--- Home/.config/fish/conf.d/vx.fish
+++ Home/.config/fish/conf.d/vx.fish
@@ -1,2 +1,19 @@
-vx shell completions fish --use-system-path | source
-vx shell init fish --use-system-path | source
+vx shell completions fish | source
+vx shell init fish | source
+
+uv python update-shell
+uv venv
+
+source "$HOME/.venv/bin/activate.fish"
+
+vx install --force uv latest
+vx install --force uvx latest
+vx install --force cargo latest
+vx install --force npm latest
+vx install --force npx latest
+vx install --force nodejs latest
+vx install --force node latest
+vx install --force go latest
+vx install --force golang latest
+
+
