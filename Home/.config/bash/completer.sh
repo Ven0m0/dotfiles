f93779b812377750a10170c379bbfbc032f81a0f
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-25 03:34:33 +0200

    Update completer.sh

--- Home/.config/bash/completer.sh
+++ Home/.config/bash/completer.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/bash
-SHELL=/usr/bin/bash 
+SHELL=/usr/bin/bash
 
-load_completion() {
+has() { command -v -- "$1" &>/dev/null; }
+ifsource(){ [[ -r "${1/#\~\//${HOME}/}" ]] && . "${1/#\~\//${HOME}/}" &>/dev/null; }
+
+# name: function name (_cargo, _git, _gh…)
+# cmd:  command to check (cargo, git, gh…)
+# kind: eval|source
+# src:  string to eval or file to source
+load_completion(){
   local name=$1 cmd=$2 kind=$3 src=$4
-  # name: function name (_cargo, _git, _gh…)
-  # cmd:  command to check (cargo, git, gh…)
-  # kind: eval|source
-  # src:  string to eval or file to source
-  command -v "$cmd" &>/dev/null || return
-  declare -F "$name" &>/dev/null && return
+  has "$cmd" || return
+  declare -F "$name" >/dev/null 2>&1 && return
   case $kind in
     eval) eval "$src" ;;
-    source) . "$src" ;;
+    source) ifsource "$src" ;;
   esac
 }
 
 # Git
 load_completion _git git source /usr/share/bash-completion/completions/git
-
 # GitHub CLI
-load_completion _gh gh eval  "$(gh completion -s bash)"
+load_completion _gh gh eval "$(gh completion -s bash)"
   
 # Rustup + Cargo
 load_completion _rustup rustup eval "$(rustup completions bash rustup)"
 load_completion _cargo  cargo  eval "$(rustup completions bash cargo)"
+
