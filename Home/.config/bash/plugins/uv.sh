4d9c51c429c5fe18e131cf26af90e753beda4fb3
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-27 10:31:04 +0200

    Update uv.sh

--- Home/.config/bash/plugins/uv.sh
+++ Home/.config/bash/plugins/uv.sh
@@ -1,7 +1,7 @@
 if command -v uv &>/dev/null; then
-  export UV_COMPILE_BYTECODE=1
-  export UV_NATIVE_TLS=1
-  export UV_CONCURRENT_BUILDS=$(nproc)
+  export UV_CONCURRENT_BUILDS=$(nproc) UV_CONCURRENT_INSTALLS=$(nproc)
   export UV_CONCURRENT_DOWNLOADS=$(( $(nproc) / 2 ))
-  export UV_CONCURRENT_INSTALLS=$(nproc)
+  export UV_NO_VERIFY_HASHES=1 UV_LINK_MODE=hardlink UV_COMPILE_BYTECODE=1
+  export UV_SYSTEM_PYTHON=1 UV_BREAK_SYSTEM_PACKAGES=0 UV_TORCH_BACKEND=auto
+  export UV_FORK_STRATEGY=fewest UV_RESOLUTION=highest UV_PRERELEASE="if-necessary-or-explicit"
 fi
