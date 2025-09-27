0bf1f836c2bfa2a4b10dc9268d5c18c682615a92
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-27 07:19:49 +0200

    Update colors.bash

--- Home/.config/bash/plugins/colors.bash
+++ Home/.config/bash/plugins/colors.bash
@@ -3,3 +3,15 @@ if command -v vivid &> /dev/null; then
     export VIVID_THEME="molokai"
     export LS_COLORS="$(vivid generate)"
 fi
+
+# Color for manpages in less makes manpages a little easier to read
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;31m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;32m'
+
+# colored GCC warnings and errors
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
