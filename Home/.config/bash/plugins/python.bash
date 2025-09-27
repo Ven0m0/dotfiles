3144ed1adc59ce8a7daf2a479272f9e16e4e434f
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-27 07:20:33 +0200

    Update python.bash

--- Home/.config/bash/plugins/python.bash
+++ Home/.config/bash/plugins/python.bash
@@ -3,6 +3,6 @@ if command -v pypy3 &>/dev/null; then
   unset PYPY_DISABLE_JIT PYPYLOG
 fi
 if command -v python3 &>/dev/null; then
-  export PYTHONOPTIMIZE=2
+  export PYTHONOPTIMIZE=2 PYTHONIOENCODING='UTF-8' PYTHON_JIT=1 PYTORCH_ENABLE_MPS_FALLBACK=1
 fi
   
