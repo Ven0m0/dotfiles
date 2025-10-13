061920f6294609ad7e8d04308264f7ebf447c1cf
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-10-14 01:20:19 +0200

    Update init.lua

--- Home/.config/xplr/init.lua
+++ Home/.config/xplr/init.lua
@@ -7,6 +7,9 @@ package.path = home
 .. "/.config/xplr/plugins/?.lua;"
 .. package.path
 
+require("ui").setup()
+require("keys").setup()
+require("plug").setup()
 xplr.config.general.disable_debug_error_mode = true
 xplr.config.general.enable_mouse = true
 xplr.config.general.show_hidden = true
