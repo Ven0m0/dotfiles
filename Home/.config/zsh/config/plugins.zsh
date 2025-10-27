f44b461050996f206700e4be9e8b2a2eec3da098
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-10-27 01:50:54 +0100

    Create plugins.zsh

---
+++ Home/.config/zsh/config/plugins.zsh
@@ -0,0 +1,25 @@
+# Antidote plugin list (one per line). Use repo[@ref] [key:value ...].
+zsh-users/zsh-completions
+zdharma-continuum/fast-syntax-highlighting
+zsh-users/zsh-autosuggestions
+marlonrichert/zsh-autocomplete
+romkatv/powerlevel10k
+# FZF tab completion (provides fzf_completion function)
+lincheney/fzf-tab-completion pick:zsh/fzf-zsh-completion.sh
+# OMZ snippets (small files only)
+ohmyzsh/ohmyzsh path:plugins/git
+ohmyzsh/ohmyzsh path:plugins/sudo
+ohmyzsh/ohmyzsh path:plugins/archlinux
+ohmyzsh/ohmyzsh path:plugins/debian
+ohmyzsh/ohmyzsh path:plugins/docker
+ohmyzsh/ohmyzsh path:plugins/docker-compose
+ohmyzsh/ohmyzsh path:plugins/ssh-agent
+ohmyzsh/ohmyzsh path:plugins/command-not-found
+# Small utility plugins
+hlissner/zsh-autopair
+MichaelAquilina/zsh-you-should-use
+romkatv/zsh-defer
+QuarticCat/zsh-smartcache
+qoomon/zsh-lazyload
+psprint/zsh-sweep
+adi-li/zsh-cwebpb
