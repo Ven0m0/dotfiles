08fb456f6a026df66db63578a0de34357eb87c92
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-01 04:32:47 +0100

    Create flake.nix

---
+++ Home/.config/nix/flake.nix
@@ -0,0 +1,8 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+}
