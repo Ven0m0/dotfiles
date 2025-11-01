6291652da061f0f88770a9eef478998269a46784
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-01 19:36:24 +0100

    Update config.fish

--- Home/.config/fish/config.fish
+++ Home/.config/fish/config.fish
@@ -1,57 +1,49 @@
+status -i || exit 0
+if not status --is-interactive
+    return
+end
 source /usr/share/cachyos-fish-config/cachyos-config.fish
-set -e LC_ALL
 
-function init_tool
-	if type -q $argv[1]
-      eval "$argv[2]"
-  end
+set -g fish_greeting
+
+if type -q _evalcache
+	function init_tool
+		if type -qf $argv[1]
+      		_evalcache "$argv[2] 2>/dev/null"
+  		end
+	end
+else
+	function init_tool
+		if type -qf $argv[1]
+			eval "$argv[2] 2>/dev/null" 
+		end
+	end
 end
 
-# Only for interactive shells
-if status -i >/dev/null 2>&1
 
-	set -g fish_greeting
+#set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
-	#set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -U __done_notification_urgency_level low
+set -x GPG_TTY (tty)
+set -gx COLORTERM truecolor
 
-	set -U __done_notification_urgency_level low
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/bin
+fish_add_path $HOME/.bin
+fish_add_path $HOME/bun/bin
 
-	#set -gx COLORTERM truecolor
+set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
+set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 
-	#fish_add_path $HOME/.local/bin
-	#fish_add_path $HOME/.cargo/bin
-
-	set -x GPG_TTY (tty)
-	init_tool fzf "fzf --fish | source"
-	init_tool starship init fish | source
-	init_tool zoxide "zoxide init --cmd cd fish | source"
-	init_tool mise "mise activate fish | source"
-	init_tool navi "navi widget fish | source"
-
-	#set -gx ZSTD_CLEVEL 19
-	set -gx ZSTD_NBTHREADS $(nproc --ignore=1)"
-	# set -gx ZSTD_NBTHREADS (math (nproc)/2)
-	#set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
-	#set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
-
-	eval (zellij setup --generate-auto-start fish | string collect)
-	intelli-shell init fish | source
-
-end
+init_tool fzf "fzf --fish"
+init_tool starship "starship init fish"
+init_tool zoxide "zoxide init --cmd cd fish"
+init_tool mise "mise activate fish"
+init_tool navi "navi widget fish"
+init_tool zellij "zellij setup --generate-auto-start fish | string collect"
+init_tool intelli-shell "intelli-shell init fish"
+init_tool cod "cod init $fish_pid fish"
 
 # ─── Path Deduplication ─────────────────────────────────────────────────────────
 set PATH (printf "%s" "$PATH" | awk -O -v RS=':' '!a[$1]++ { if (NR > 1) printf RS; printf $1 }')
-# Deduplicate PATH (preserve order) to prevent PATH bloat across reloads
-#set -l seen
-#set -l newpath
-#for dir in $PATH
-  #if not contains -- $dir $seen 2>/dev/null
-    #set seen $seen $dir
-    #set newpath $newpath $dir
-  #end
-#end
-#set -gx PATH $newpath
-
-if type -qf zoxide
-	_evalcache zoxide init fish 2>/dev/null
-end
