2ac6cfd7035b6af8e1909f1236df1efa192b7cb6
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-01 19:37:48 +0100

    Update starship.fish

--- Home/.config/fish/conf.d/starship.fish
+++ Home/.config/fish/conf.d/starship.fish
@@ -2,7 +2,7 @@ function starship_transient_prompt_func
   starship module character
 end
 function starship_transient_rprompt_func
-  starship module time
+    starship module time
 end
-starship init fish | source
+_evalcache starship init fish
 enable_transience
