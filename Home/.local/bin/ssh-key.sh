25489d5c22ad2b54d6c920e68ea668b677fe1ec8
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-04 19:59:18 +0100

    Delete Home/.local/bin/ssh-key.sh

--- Home/.local/bin/ssh-key.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# SSH key generation and deployment script
-
-ssh-key(){
-  email="${email:=${1:-ven0m0.wastaken@gmail.com}}"
-  target="${2:=dietpi@192.168.178.86}"
-  ssh-keygen -t ed25519 -a 100 -f ~/.ssh/pi_ed25519 -C "$email"
-  ssh-copy-id -i ~/.ssh/pi_ed25519.pub "$target"
-}
