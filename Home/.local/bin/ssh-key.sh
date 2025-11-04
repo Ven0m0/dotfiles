41830b6cfaf142dc7138353e43986e1938a4a006
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-04 19:58:31 +0100

    Update ssh-key.sh

--- Home/.local/bin/ssh-key.sh
+++ Home/.local/bin/ssh-key.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # SSH key generation and deployment script
-email=""
-ssh-keygen -t ed25519 -a 100 -f ~/.ssh/pi_ed25519 -C "${email}"
-target=""
-ssh-copy-id -i ~/.ssh/pi_ed25519.pub dietpi@192.168.178.86
+
+ssh-key(){
+  email="${email:=${1:-ven0m0.wastaken@gmail.com}}"
+  target="${2:=dietpi@192.168.178.86}"
+  ssh-keygen -t ed25519 -a 100 -f ~/.ssh/pi_ed25519 -C "$email"
+  ssh-copy-id -i ~/.ssh/pi_ed25519.pub "$target"
+}
