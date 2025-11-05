7892eba741d7bee909f7056fa03e122a84549630
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-05 01:49:04 +0100

    Update ls.fish

--- Home/.config/fish/functions/ls.fish
+++ Home/.config/fish/functions/ls.fish
@@ -1,3 +1,3 @@
 function ls --wraps='eza -G --color --group-directories-first --icons --no-time --no-user --no-permissions --git-repos-no-status' --description 'alias ls=eza -G --color --group-directories-first --icons --no-time --no-user --no-permissions --git-repos-no-status'
   eza -G --color --group-directories-first --icons --no-time --no-user --no-permissions --git-repos-no-status $argv
-ende
+end
