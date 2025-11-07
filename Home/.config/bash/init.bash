0a23c53c8bbf1feb0f77963da00d88d7cf59f399
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-07 06:50:40 +0100

    Update init.bash

--- Home/.config/bash/init.bash
+++ Home/.config/bash/init.bash
@@ -14,6 +14,11 @@ shopt -s nullglob
 for plugin in "$BASH_CONFIG_DIR"/plugins/*.{bash,sh}; do
   . "$plugin"
 done
+
+# --- Completions
+ifsource ~/.config/bash/completer.sh
+ifsource ~/.config/bash/completions/fzf-completion.sh
+
 # --- Initialize Lazy Loading for Functions
 # Scans function directories and creates stubs for on-demand loading.
 lazy_init "$BASH_CONFIG_DIR/functions"
