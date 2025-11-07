0d3a1928d0fdc2a513626441e840cdd88a0e1285
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-07 06:48:19 +0100

    Update completer.sh

--- Home/.config/bash/completer.sh
+++ Home/.config/bash/completer.sh
@@ -1,33 +1,50 @@
 #!/usr/bin/bash
-SHELL=/usr/bin/bash
+# Completion loader - lazy-load system/tool completions
 
-has() { command -v -- "$1" &>/dev/null; }
-ifsource(){ [[ -r "${1/#\~\//${HOME}/}" ]] && . "${1/#\~\//${HOME}/}" &>/dev/null; }
+has(){ command -v "$1" &>/dev/null; }
 
-# name: function name (_cargo, _git, _gh…)
-# cmd:  command to check (cargo, git, gh…)
-# kind: eval|source
-# src:  string to eval or file to source
 load_completion(){
   local name=$1 cmd=$2 kind=$3 src=$4
   has "$cmd" || return
-  declare -F "$name" >/dev/null 2>&1 && return
+  declare -F "$name" &>/dev/null && return
   case $kind in
     eval) eval "$src" ;;
-    source) ifsource "$src" ;;
+    source) [[ -r ${src/#\~\//${HOME}/} ]] && . "${src/#\~\//${HOME}/}" &>/dev/null ;;
   esac
 }
 
-# Git
+# System completions - extend below
 load_completion _git git source /usr/share/bash-completion/completions/git
-# GitHub CLI
-load_completion _gh gh eval "$(gh completion -s bash)"
-  
-# Rustup + Cargo
-load_completion _rustup rustup eval "$(rustup completions bash rustup)"
-load_completion _cargo  cargo  eval "$(rustup completions bash cargo)"
+# Tool completions - add your own
+has gh && load_completion _gh gh eval "$(gh completion -s bash)"
+has rustup && {
+  load_completion _rustup rustup eval "$(rustup completions bash rustup)"
+  load_completion _cargo cargo eval "$(rustup completions bash cargo)"
+}
 
-# NOTE: These downloads should be run during setup, not at shell sourcing time
-# Run manually or add to setup script:
-# curl -sf https://raw.githubusercontent.com/endeavouros-team/PKGBUILDS/master/reflector-bash-completion/reflector-bash-completion -o "${HOME}/.config/bash/completions/reflector.bash"
-# curl -sf -O https://raw.githubusercontent.com/trapd00r/LS_COLORS/master/lscolors.sh
+# Editor completions - specialized fullscreen picker
+_editor_completion(){
+  bind '"\e[0n": redraw-current-line' 2>/dev/null
+  local selected
+  if selected=$(compgen -f -- "${COMP_WORDS[COMP_CWORD]}" | fzf \
+    --prompt='❯ ' --height=~100% \
+    --tiebreak=begin,index \
+    -1 -0--exact --layout=reverse \
+    --bind=tab:down,btab:up --cycle); then
+    [[ -d $selected ]] && selected="${selected}/" || selected="${selected} "
+    COMPREPLY=( "$selected" )
+  fi
+  printf '\e[5n'
+}
+
+# Register editors - extend array as needed
+declare -a EDITOR_CMDS=( "${EDITOR:-}" nano micro vi vim code)
+for _cmd in "${EDITOR_CMDS[@]}"; do
+  has "$_cmd" && complete -o nospace -F _editor_completion "$_cmd"
+done
+unset _cmd
+
+# Custom completions - add below
+# Example:
+# has kubectl && load_completion _kubectl kubectl source <(kubectl completion bash)
+# has docker && load_completion _docker docker source /usr/share/bash-completion/completions/docker
