66c7b5d424204e8dc13ddb815b6a1c433a7d9d45
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-07 07:47:58 +0100

    Update env.sh

--- Home/.config/bash/env.sh
+++ Home/.config/bash/env.sh
@@ -6,17 +6,13 @@ HOME="/home/${SUDO_USER:-$USER}"
 builtin cd -- "$(command dirname -- "${BASH_SOURCE[0]:-$0}")" || exit 1
 jobs="$(command nproc --all 2>/dev/null || echo 4)"
 #–– Colors
-# A single function to generate any ANSI code on demand
-C(){
-  local -rA s=([def]=0 [bld]=1 [dim]=2 [und]=4 [inv]=7 [hid]=8 \
-    [blk]=30 [red]=31 [grn]=32 [ylw]=33 [blu]=34 [mgn]=35 [cyn]=36 [wht]=37)
-  local n
-  case "$1" in
-    b_*) n=${1#b_}; [[ -v s[$n] ]] && printf '\e[%sm' $((60+s[$n])) ;;
-    bg_*) n=${1#bg_}; [[ -v s[$n] ]] && printf '\e[%sm' $((10+s[$n])) ;;
-    b_bg_*) n=${1#b_bg_}; [[ -v s[$n] ]] && printf '\e[%sm' $((70+s[$n])) ;;
-    c_*) printf '\e[38;5;%sm' "${1#c_}" ;;
-    bg_c_*) printf '\e[48;5;%sm' "${1#bg_c_}" ;;
-    *) [[ -v s[$1] ]] && printf '\e[%sm' "${s[$1]}" ;;
-  esac
-}
+# Pre-gen common colors
+declare -rA _c=([def]=0 [bld]=1 [dim]=2 [und]=4 [inv]=7 [hid]=8 \
+  [blk]=30 [red]=31 [grn]=32 [ylw]=33 [blu]=34 [mgn]=35 [cyn]=36 [wht]=37)
+for c in "${!_c[@]}"; do
+  printf -v ${c^^} '\e[%sm' ${_c[$c]}
+  printf -v B_${c^^} '\e[%sm' $((60+_c[$c]))
+  printf -v BG_${c^^} '\e[%sm' $((40+_c[$c]))
+done
+# Function for 256-colors only
+C(){ printf '\e[%d;5;%sm' $([[ $1 == bg_* ]] && echo 48 || echo 38) ${1#*_}; }
