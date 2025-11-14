6088ac57568c2f97885ec103d2775aac8be4ae28
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-15 00:55:28 +0100

    Update bat.sh

--- Home/.config/bash/plugins/bat.sh
+++ Home/.config/bash/plugins/bat.sh
@@ -1,22 +1,17 @@
 # https://github.com/ethanjli/dotfiles/blob/master/dot_config/bash/integrations/20-bat.basic.sh
+LESS_TERMCAP_md=$'\e[01;31m' LESS_TERMCAP_me=$'\e[0m' LESS_TERMCAP_us=$'\e[01;32m' LESS_TERMCAP_ue=$'\e[0m' LESS_TERMCAP_so=$'\e[45;93m' LESS_TERMCAP_se=$'\e[0m'
+LESS='less -RFKQiqs --use-color --no-histdups --no-edit-warn -Dd+r$Du+b$'
+export LESSCHARSET=utf-8 LESSHISTFILE=- LESSQUIET=1 LESS LESS_TERMCAP_md LESS_TERMCAP_me LESS_TERMCAP_us LESS_TERMCAP_ue LESS_TERMCAP_so LESS_TERMCAP_se
 if command -v bat &>/dev/null; then
-  export MANROFFOPT="-c"
-  export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-  export PAGER='bat -p -s --squeeze-limit 0' BAT_PAGER='less -RFQs --use-color --no-histdups --mouse --wheel-lines=2'
-  alias cat='\bat -pp -s --squeeze-limit 0'
-  unalias bat 2>/dev/null || true
-  export BAT_STYLE=auto LESSQUIET=1 BATDIFF_USE_DELTA=true BATPIPE=color
-  export LESSCHARSET='utf-8' LESSHISTFILE=-
-  export LESS_TERMCAP_md=$'\e[01;31m' LESS_TERMCAP_me=$'\e[0m' LESS_TERMCAP_us=$'\e[01;32m' LESS_TERMCAP_ue=$'\e[0m' LESS_TERMCAP_so=$'\e[45;93m' LESS_TERMCAP_se=$'\e[0m'
-  if command -v batman &>/dev/null; then
-    eval "$(batman --export-env)"
-    alias man=batman
+  export PAGER='bat -p' BAT_PAGER="$LESS" MANROFFOPT='-c'
+  export BAT_STYLE="-numbers,-header-filename" BATDIFF_USE_DELTA=true BATPIPE=color
+  if has batman; then
+    export MANPAGER='env BATMAN_IS_BEING_MANPAGER=yes bash /usr/bin/batman'; alias man=batman
   else
-    export MANPAGER="sh -c 'col -bx | bat -lman -p -s --squeeze-limit 0'"
+    export MANPAGER="sh -c 'col -bx | bat -plman'"
   fi
-  command -v batpipe &>/dev/null && eval "$(SHELL=bash batpipe)"
-else
-  alias cat='cat -sn'
-  export LESS_TERMCAP_md=$'\e[01;31m' LESS_TERMCAP_me=$'\e[0m' LESS_TERMCAP_us=$'\e[01;32m' LESS_TERMCAP_ue=$'\e[0m' LESS_TERMCAP_so=$'\e[45;93m' LESS_TERMCAP_se=$'\e[0m'
-  export LESSHISTFILE=- LESSCHARSET=utf-8 PAGER="${PAGER:-less}" LESS='less -RFQs --use-color --no-histdups --mouse --wheel-lines=2'
+  has batpipe && { export LESSOPEN="|/usr/bin/batpipe %s" BATPIPE=color; unset LESSCLOSE; }
+  eval $(bat --completion bash)
+  alias cat='bat -pp'
 fi
+export PAGER="${PAGER:-less}"
