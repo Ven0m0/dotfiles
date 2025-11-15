a3bdba28679fc385edf8161d9e60bc64caf2520b
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-15 02:14:52 +0100

    Update 02-tools.bash

--- Home/.config/bash/plugins/02-tools.bash
+++ Home/.config/bash/plugins/02-tools.bash
@@ -2,12 +2,19 @@
 # --- Language & Runtimes
 has mise && eval "$(mise activate -yq bash)"
 ifsource "$HOME/.sdkman/bin/sdkman-init.sh"
-has cargo && {
+if has cargo; then
   exportif RUSTUP_HOME "$HOME/.rustup"
   exportif CARGO_HOME "$HOME/.cargo"
   ifsource "$CARGO_HOME/env"
   prependpath "$CARGO_HOME/bin"
-}
+fi
+has sccache && export RUSTC_WRAPPER=sccache
+
+has go && export CGO_ENABLED=0 GOOS=linux GOARCH=amd64
+
+export HOMEBREW_NO_ANALYTICS=true
+
+export JAVA_OPTIONS="${JAVA_OPTIONS:-'-Dfile.encoding=UTF-8 -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true'}"
 
 # --- Shell Enhancement Tools
 has gh && eval "$(gh completion -s bash)"
@@ -17,3 +24,6 @@ has zoxide && {
 }
 has zellij && eval "$(zellij setup --generate-auto-start bash)"
 [[ "$TERM" == "xterm-ghostty" ]] && ifsource "${GHOSTTY_RESOURCES_DIR:-}/shell-integration/bash/ghostty.bash"
+
+has thefuck && eval "$(thefuck --alias)"
+has pay-respects && eval "$(pay-respects bash)"
