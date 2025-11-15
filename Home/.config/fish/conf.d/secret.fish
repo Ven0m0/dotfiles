2f9417aa22bfee25c45898c1954c75c7ff015a72
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-15 10:40:19 +0100

    Update secret.fish

--- Home/.config/fish/conf.d/secret.fish
+++ Home/.config/fish/conf.d/secret.fish
@@ -2,7 +2,5 @@ status -i >/dev/null 2>&1 || return
 # you see nothing...
 set -gx SHELL_MOMMYS_ONLY_NEGATIVE true
 set -gx MOMMY_SWEETIE girl
-alias margo='cargo mommy '
-alias gargo='cargo gg '
 set -gx CARGO_MOMMYS_MOODS chill/thirsty/yikes
 set -gx CARGO_MOMMYS_FUCKING pet/slut/toy/whore
