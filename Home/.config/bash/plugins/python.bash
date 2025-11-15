a6299d3da5042e8b5b5d0d2c614270d238e51ff9
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-15 11:17:27 +0100

    Update python.bash

--- Home/.config/bash/plugins/python.bash
+++ Home/.config/bash/plugins/python.bash
@@ -1,6 +1,7 @@
 # Python settings (main config in bashenv.env)
 export PYTHONOPTIMIZE=2 PYTHONIOENCODING='UTF-8' PYTHON_JIT=1 PYTHON_DISABLE_REMOTE_DEBUG=1 PYTORCH_ENABLE_MPS_FALLBACK=1 PYENV_VIRTUALENV_DISABLE_PROMPT=1
 
+# Use uv for pip operations when available
 pip(){
   if has uv && [[ "install uninstall list show freeze check" =~ "$1" ]]; then
     command uv pip "$@"
