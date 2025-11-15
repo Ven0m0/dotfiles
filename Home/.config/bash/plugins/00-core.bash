994eadcacc41e56434268aaffa5e1d47cbd79c93
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-15 01:13:42 +0100

    Update 00-core.bash

--- Home/.config/bash/plugins/00-core.bash
+++ Home/.config/bash/plugins/00-core.bash
@@ -2,16 +2,15 @@
 # --- History
 HISTCONTROL="erasedups:ignoreboth" HISTSIZE=5000 HISTFILESIZE=10000
 HISTIGNORE="&:bg:fg:clear:cls:exit:history:?"
-HISTTIMEFORMAT="%F %T " HISTFILE="$HOME/.bash_history"
-PROMPT_DIRTRIM=3
-shopt -s histappend cmdhist
+HISTTIMEFORMAT="%F %T " HISTFILE="${HOME}/.bash_history"
+PROMPT_DIRTRIM=3 PROMPT_COMMAND="history -a"
 
 # --- Shell Behavior
-shopt -s autocd cdable_vars cdspell checkwinsize dirspell extglob globstar hostcomplete no_empty_cmd_completion nullglob
+shopt -s autocd cdable_vars cdspell checkwinsize dirspell globstar nullglob hostcomplete no_empty_cmd_completion histappend cmdhist
 set -o noclobber
 bind -r '\C-s'
 stty -ixon -ixoff -ixany
-export IGNOREEOF=10
+export IGNOREEOF=10 COLUMNS
 
 # --- Sourcing
 ifsource /etc/bashrc
