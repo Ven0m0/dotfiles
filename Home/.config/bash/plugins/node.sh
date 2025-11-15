734d2ec218fac537985330b7ded05b839cddd217
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-15 10:57:05 +0100

    Update node.sh

--- Home/.config/bash/plugins/node.sh
+++ Home/.config/bash/plugins/node.sh
@@ -1,5 +1,7 @@
 if command -v bun &>/dev/null; then
-  alias npx=bunx
+  alias npx=bunx npm=bun
+  if [[ -n "$BUN_INSTALL" ]]; then
+    export BUN_INSTALL "$HOME/.bun"
+    prependpath "$BUN_INSTALL/bin"
 fi
-
 export NODE_OPTIONS="--no-warnings --max-old-space-size=4096"
