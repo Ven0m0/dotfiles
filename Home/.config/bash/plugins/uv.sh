4016a4abe881563e127fcc2b8cccba18e09d4154
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-15 01:23:43 +0100

    Update uv.sh

--- Home/.config/bash/plugins/uv.sh
+++ Home/.config/bash/plugins/uv.sh
@@ -1,7 +1,7 @@
-if command -v uv &>/dev/null; then
-  export UV_CONCURRENT_BUILDS=$(nproc) UV_CONCURRENT_INSTALLS=$(nproc)
-  export UV_CONCURRENT_DOWNLOADS=$(( $(nproc) / 2 ))
-  export UV_NO_VERIFY_HASHES=1 UV_LINK_MODE=hardlink UV_COMPILE_BYTECODE=1
-  export UV_SYSTEM_PYTHON=1 UV_BREAK_SYSTEM_PACKAGES=0 UV_TORCH_BACKEND=auto
-  export UV_FORK_STRATEGY=fewest UV_RESOLUTION=highest UV_PRERELEASE="if-necessary-or-explicit"
-fi
+pip(){ 
+  if command uv &>/dev/null && [[ "install uninstall list show freeze check" =~ "$1" ]]; then
+    uv pip "$@"
+  else 
+    command python -m pip "$@"
+  fi
+}
