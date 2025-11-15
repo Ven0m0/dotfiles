712d67b285df48bfc3f17216d5a3cfdde91fba36
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-15 10:29:13 +0100

    Update starship.fish

--- Home/.config/fish/conf.d/starship.fish
+++ Home/.config/fish/conf.d/starship.fish
@@ -1,14 +1,14 @@
 status -i >/dev/null 2>&1 || return
-type -q starship >/dev/null 2>&1 || return
+type -q starship || return
 function starship_transient_prompt_func
-  starship module character
+  starship module character 2>/dev/null
 end
 function starship_transient_rprompt_func
-    starship module time
+    starship module time 2>/dev/null
 end
 if type -q _evalcache
-  _evalcache starship init fish
+  _evalcache starship init fish 2>/dev/null
 else
-  starship init fish | source
+  starship init fish 2>/dev/null | source 2>/dev/null
 end
-enable_transience
+enable_transience >/dev/null 2>&1
