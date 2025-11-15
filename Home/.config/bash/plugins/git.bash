8d17176fcc0d48daa32296667d5f29c92842927b
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-15 01:37:06 +0100

    Update git.bash

--- Home/.config/bash/plugins/git.bash
+++ Home/.config/bash/plugins/git.bash
@@ -1,8 +1,13 @@
-#================================= [Git Funcs] ================================
-if has gix; then
-  gclone(){ command gix clone --depth 1 --no-tags "$@"; }
-else
-  gclone(){ command git clone --filter=blob:none --depth 1 --no-tags -c protocol.version=2 "$@"; }
-fi
-
+LC_ALL=C LANG=C
 gpush(){ command git add -A && command git commit -m "${1:-Update}" && command git push --recurse-submodules=on-demand; }
+
+gctl(){
+  [[ $# -eq 0 ]] && { echo "Usage: gctl <git-repo-url> [directory]" >&2; return 1; }; local dir url="$1"
+  if [[ -n "$2" ]]; then dir="$2"; else dir="$(basename "${url%%/}")"; dir="${dir%.git}"; fi
+  if command -v gix &>/dev/null [[ ! -d "$dir" ]]; then
+    command gix clone --depth 1 --no-tags "$url" "$dir" || return 1
+  elif command -v git &>/dev/null && [[ ! -d "$dir" ]]; then
+    command git clone --depth 1 --no-tags --filter='blob:none' --also-filter-submodules --shallow-submodules -c protocol.version=2 "$url" "$dir" || return 1
+  fi
+  [[ -d "$dir" ]] && { cd -- "$dir" || return; command git pull; return 0; } 
+}
