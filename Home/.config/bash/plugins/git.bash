3990f909d5328d5fc375ded0f7c884fd8df414ae
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-15 11:04:02 +0100

    Update git.bash

--- Home/.config/bash/plugins/git.bash
+++ Home/.config/bash/plugins/git.bash
@@ -7,15 +7,16 @@ gpush(){
 }
 
 gctl(){
-  [[ $# -eq 0 ]] && { echo "Usage: gctl <git-repo-url> [directory]" >&2; return 1; }; local dir url="$1"
-  if [[ -n "$2" ]]; then dir="$2"; else dir="$(basename "${url%%/}")"; dir="${dir%.git}"; fi
-  if command -v gix &>/dev/null [[ ! -d "$dir" ]]; then
-    command gix clone --depth 1 --no-tags -c protocol.version=2 -c http.version=HTTP/2 \
-    -c http.sslVersion=tlsv1.3 "$url" "$dir" || return 1
-  elif command -v git &>/dev/null && [[ ! -d "$dir" ]]; then
-    command git clone --depth 1 --no-tags --filter='blob:none' --also-filter-submodules \
-      --shallow-submodules -c protocol.version=2 -c http.version=HTTP/2 -c http.sslVersion=tlsv1.3 \
-      "$url" "$dir" || return 1
+  [[ $# -eq 0 ]] && { echo "Usage: gctl <git-repo-url> [directory]" >&2; return 1; }; local url="$1" dir="$2"
+  if [[ ! -n "$2" ]]; then dir="$(basename "${url%%/}")"; dir="${dir%.git}"; fi
+  if [[ ! -d "$dir" ]]; then
+    if has gix; then
+      command gix clone --depth 1 --no-tags -c protocol.version=2 -c http.version=HTTP/2 -c http.sslVersion=tlsv1.3 "$url" "$dir" || return 1
+    else
+      command git clone --depth 1 --no-tags --filter='blob:none' --also-filter-submodules --shallow-submodules \
+        -c protocol.version=2 -c http.version=HTTP/2 -c http.sslVersion=tlsv1.3 "$url" "$dir" || return 1
+    fi
+  else
+    [[ -d "$dir" ]] && { cd -- "$dir" || return; command git pull -c protocol.version=2 -c http.version=HTTP/2 -c http.sslVersion=tlsv1.3; return 0; }
   fi
-  [[ -d "$dir" ]] && { cd -- "$dir" || return; command git pull -c protocol.version=2 -c http.version=HTTP/2 -c http.sslVersion=tlsv1.3; return 0; } 
 }
