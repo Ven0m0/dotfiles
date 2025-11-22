cbfd412d52b341fa3c36fce8585f8749ae4859cd
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-22 11:46:08 +0100

    Update pkgui.sh

--- Home/.local/bin/pkgui.sh
+++ Home/.local/bin/pkgui.sh
@@ -7,7 +7,7 @@ readonly RED=$'\e[31m' BLD=$'\e[1m' DEF=$'\e[0m' UL=$'\e[4m'
 has(){ command -v "$1" &>/dev/null; }
 err(){ printf '%b[ERR]%b %s\n' "${RED}" "${DEF}" "$*" >&2; }
 die(){ err "$@"; exit 1; }
-_ver(){ printf '%b%s%b -- %bv3.0.1%b unified pacman/paru TUI\n' "${BLD}" "${0##*/}" "${DEF}" "${UL}" "${DEF}"; }
+_ver(){ printf '%b%s%b -- %bv3.1.0%b unified pacman/paru TUI (merged with fzpacman)\n' "${BLD}" "${0##*/}" "${DEF}" "${UL}" "${DEF}"; }
 _help(){
   cat <<EOF
 ${BLD}USAGE${DEF}  ${0##*/} ${UL}CMD${DEF} [${UL}KEYWORDS${DEF}]
@@ -16,6 +16,7 @@ ${BLD}COMMANDS${DEF}
   s         Search packages
   l         List local packages
   S         Search and install
+  D         Search and download only
   R         List and remove packages
   A         List and remove orphans
   u         Update package list
@@ -23,11 +24,19 @@ ${BLD}COMMANDS${DEF}
   -h, h     Show this help
   -v, v     Show version
 
-${BLD}KEYS${DEF}   Tab:select Enter:confirm Ctrl-S:info Ctrl-J/K:nav
+${BLD}KEYS${DEF}
+  Tab        Select/multi-select
+  Enter      Confirm action
+  Ctrl-S     View detailed info
+  Ctrl-O     Download only (in search mode)
+  Ctrl-J/K   Navigate preview
+  Alt-P      Toggle preview
+  Alt-J/K    Scroll preview
 
 ${BLD}EXAMPLES${DEF}
   ${0##*/} s firefox       Search for firefox
   ${0##*/} S firefox       Search and install firefox
+  ${0##*/} D firefox       Search and download firefox (no install)
   ${0##*/} l               Browse installed packages
   ${0##*/} R firefox       Remove firefox
   ${0##*/} A               Remove orphaned packages
@@ -42,14 +51,16 @@ for f in ${FINDER:-sk fzf}; do has "${f}" && FND="${f}" && break; done
 [[ -z ${FND:-} ]] && die "No fuzzy finder found (sk/fzf)"
 # Cache for package info
 declare -A _CACHE_INFO _CACHE_INFOQ _CACHE_LIST _CACHE_LOCALLIST
+
 _fzf(){
-  local -a opts=(--ansi --cycle --no-mouse --reverse --inline-info)
+  local -a opts=(--ansi --cycle --no-mouse --reverse --inline-info --color='pointer:green,marker:green')
   [[ ${FND} == sk ]] && opts+=(--no-hscroll) || opts+=(--no-scrollbar)
   while (($#)); do
     case "$1" in
       -m) opts+=(-m); shift ;;
       -h) opts+=(--header "$2"); shift 2 ;;
-      -p) opts+=(--preview "$2" --preview-window=wrap); shift 2 ;;
+      -p) opts+=(--preview "$2" --preview-window='down:65%:wrap'); shift 2 ;;
+      -l) opts+=(--preview-label "$2"); shift 2 ;;
       -b) opts+=(--bind "$2"); shift 2 ;;
       *) shift ;;
     esac
@@ -91,20 +102,38 @@ _getlocal(){
 
 _search(){
   export -f _info _fzf; export PAC FND; declare -gA _CACHE_INFO
-  _getlist "$@" | _fzf -m -h "Ctrl-S:info" -p "bash -c '_info {}'" \
-    -b "ctrl-s:execute(bash -c '_info {} | less -R')"
+  _getlist "$@" | _fzf -m \
+    -h $'Enter:install  Ctrl-O:download  Ctrl-S:info\nAlt-P:toggle preview  Alt-J/K:scroll' \
+    -l '[package info]' \
+    -p "bash -c '_info {}'" \
+    -b "ctrl-s:execute(bash -c '_info {} | less -R')" \
+    -b "alt-p:toggle-preview" \
+    -b "alt-d:preview-half-page-down,alt-u:preview-half-page-up" \
+    -b "alt-k:preview-up,alt-j:preview-down"
 }
 
 _local(){
   export -f _infoq _fzf; export PAC FND; declare -gA _CACHE_INFOQ
-  _getlocal "$@" | _fzf -m -h "Ctrl-S:info" -p "bash -c '_infoq {}'" \
-    -b "ctrl-s:execute(bash -c '_infoq {} | less -R')"
+  _getlocal "$@" | _fzf -m \
+    -h $'Enter:remove  Ctrl-S:info\nAlt-P:toggle preview  Alt-J/K:scroll' \
+    -l '[package info]' \
+    -p "bash -c '_infoq {}'" \
+    -b "ctrl-s:execute(bash -c '_infoq {} | less -R')" \
+    -b "alt-p:toggle-preview" \
+    -b "alt-d:preview-half-page-down,alt-u:preview-half-page-up" \
+    -b "alt-k:preview-up,alt-j:preview-down"
 }
 
 _orphans(){
   export -f _infoq _fzf; export PAC FND; declare -gA _CACHE_INFOQ
-  "${PAC}" -Qdttq "$@" 2>/dev/null | _fzf -m -h "Ctrl-S:info" -p "bash -c '_infoq {}'" \
-    -b "ctrl-s:execute(bash -c '_infoq {} | less -R')"
+  "${PAC}" -Qdttq "$@" 2>/dev/null | _fzf -m \
+    -h $'Enter:remove  Ctrl-S:info\nAlt-P:toggle preview  Alt-J/K:scroll' \
+    -l '[orphan package info]' \
+    -p "bash -c '_infoq {}'" \
+    -b "ctrl-s:execute(bash -c '_infoq {} | less -R')" \
+    -b "alt-p:toggle-preview" \
+    -b "alt-d:preview-half-page-down,alt-u:preview-half-page-up" \
+    -b "alt-k:preview-up,alt-j:preview-down"
 }
 
 _inst(){
@@ -114,6 +143,16 @@ _inst(){
     sudo pacman -S "${pkgs[@]}"
   else
     "${PAC}" -S "${pkgs[@]}"
+  fi
+}
+
+_download(){
+  local -a pkgs=(); mapfile -t pkgs
+  (( ${#pkgs[@]} == 0 )) && return 0
+  if [[ ${PAC} == pacman ]]; then
+    sudo pacman -Syw "${pkgs[@]}"
+  else
+    "${PAC}" -Syw "${pkgs[@]}"
   fi
 }
 
@@ -154,6 +193,7 @@ case "$1" in
   s) shift; _search "$@";;
   l) shift; _local "$@";;
   S) shift; _search "$@" | _inst;;
+  D) shift; _search "$@" | _download;;
   R) shift; _local "$@" | _rmv;;
   A) shift; _orphans "$@" | _rmv;;
   u) _pkglist;;
@@ -162,21 +202,3 @@ case "$1" in
   -v|v|--version) _ver;;
   *) die "Invalid command: $1";;
 esac
-
-# TODO: add "https://github.com/basecamp/omarchy/blob/master/bin/omarchy-pkg-install" functionality
-fzf_args=(
-  -m --color 'pointer:green,marker:green'
-  --preview 'pacman -Sii {1}'
-  --preview-label='alt-p: toggle description, alt-j/k: scroll, tab: multi-select'
-  --preview-label-pos='bottom' --preview-window 'down:65%:wrap'
-  --bind 'alt-p:toggle-preview'
-  --bind 'alt-d:preview-half-page-down,alt-u:preview-half-page-up'
-  --bind 'alt-k:preview-up,alt-j:preview-down'
-)
-
-pkg_names=$(pacman -Slq | fzf "${fzf_args[@]}")
-
-if [[ -n "$pkg_names" ]]; then
-  # Convert newline-separated selections to space-separated for paru
-  echo "$pkg_names" | tr '\n' ' ' | xargs sudo pacman -S --noconfirm
-fi
