afc98c5bce2ec1efcaa7114a84f04bcf83a47267
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-27 01:21:46 +0100

    Update git.bash

--- Home/.config/bash/plugins/git.bash
+++ Home/.config/bash/plugins/git.bash
@@ -1,23 +1,17 @@
 has git || return
-LC_ALL=C LANG=C
-gpush(){ 
-  command git add -A
-  command git commit -m "${1:-Update}" && command git push --recurse-submodules=on-demand \
-    -c protocol.version=2 -c http.version=HTTP/2 -c http.sslVersion=tlsv1.3
-  command git status --long
-}
+gpush(){ git add -A && { git commit -m "${1:-Update}" && LC_ALL=C git push --recurse-submodules=on-demand; }; git status; }
 
 gctl(){
   [[ $# -eq 0 ]] && { echo "Usage: gctl <git-repo-url> [directory]" >&2; return 1; }; local url="$1" dir="$2"
-  if [[ ! -n "$2" ]]; then dir="$(basename "${url%%/}")"; dir="${dir%.git}"; fi
+  [[ -n "$2" ]] || { dir="$(basename "${url%%/}")"; dir="${dir%.git}"; }
   if [[ ! -d "$dir" ]]; then
     if has gix; then
-      command gix clone --depth 1 --no-tags -c protocol.version=2 -c http.version=HTTP/2 -c http.sslVersion=tlsv1.3 "$url" "$dir" || return 1
+      LC_ALL=C gix clone --depth 1 --no-tags "$url" "$dir" || return 1
     else
-      command git clone --depth 1 --no-tags --filter='blob:none' --also-filter-submodules --shallow-submodules \
-        -c protocol.version=2 -c http.version=HTTP/2 -c http.sslVersion=tlsv1.3 "$url" "$dir" || return 1
+      LC_ALL=C git clone --depth 1 --no-tags --filter='blob:none' -c protocol.version=2 -c http.version=HTTP/2 "$url" "$dir" || return 1
     fi
   else
-    [[ -d "$dir" ]] && { cd -- "$dir" || return; command git pull -c protocol.version=2 -c http.version=HTTP/2 -c http.sslVersion=tlsv1.3; return 0; }
+    [[ -d "$dir" ]] && { cd -- "$dir" || return; LC_ALL=C git pull -c protocol.version=2 -c http.version=HTTP/2; return 0; }
   fi
+  LC_ALL=C git status; LC_ALL=C git branch -vv
 }
