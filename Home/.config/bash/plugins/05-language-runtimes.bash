d47da58baf796a7ced52d7a75800de9811570513
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-02 03:21:25 +0100

    Update 05-language-runtimes.bash

--- Home/.config/bash/plugins/05-language-runtimes.bash
+++ Home/.config/bash/plugins/05-language-runtimes.bash
@@ -15,8 +15,7 @@ if has cargo || has rustup; then
   exportif CARGO_HOME "$HOME/.cargo"
   ifsource "${CARGO_HOME:-$HOME/.cargo}/env"
   prependpath "${CARGO_HOME:-$HOME/.cargo}/bin"
-  export CARGO_HTTP_MULTIPLEXING=true CARGO_NET_GIT_FETCH_WITH_CLI=true
-  export RUST_LOG=off BINSTALL_DISABLE_TELEMETRY=1 FIGNORE=Cargo.lock
+  export CARGO_HTTP_MULTIPLEXING=true CARGO_NET_GIT_FETCH_WITH_CLI=true RUST_LOG=off BINSTALL_DISABLE_TELEMETRY=1 FIGNORE=Cargo.lock
   has sccache && export RUSTC_WRAPPER=sccache
 fi
 
@@ -25,11 +24,10 @@ has go && export GOOS=linux GOARCH=amd64 GOFLAGS="-ldflags=-s -w -trimpath -modc
 
 # --- Python
 export PYTHONOPTIMIZE=2 PYTHONIOENCODING='UTF-8' PYTHON_JIT=1
-export PYTHON_DISABLE_REMOTE_DEBUG=1 PYTORCH_ENABLE_MPS_FALLBACK=1
-export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+export PYTHON_DISABLE_REMOTE_DEBUG=1 PYTORCH_ENABLE_MPS_FALLBACK=1 PYENV_VIRTUALENV_DISABLE_PROMPT=1
 
 # Use uv for pip operations when available
-pip() {
+pip(){
   if has uv && [[ "install uninstall list show freeze check" =~ $1 ]]; then
     command uv pip "$@"
   else
@@ -38,7 +36,7 @@ pip() {
 }
 # Create and activate Python virtual environment using uv
 alias py-venv="[ -d .venv ] || uv venv .venv && source .venv/bin/activate"
-
+alias py-server='python3 -m SimpleHTTPServer 8000'
 # --- Node/Bun
 if has bun; then
   alias npx=bunx npm=bun
@@ -53,7 +51,5 @@ export SDKMAN_DIR="${SDKMAN_DIR:-$HOME/.sdkman}"
 ifsource "${SDKMAN_DIR}/bin/sdkman-init.sh"
 export JAVA_HOME="$(dirname "$(dirname "$(readlink -f "$(which java)")")")" && prependpath "${JAVA_HOME}/bin"
 
-# AppImages
-export URUNTIME_PRELOAD=1
-# https://github.com/pkgforge-dev/Citron-AppImage/issues/50
-export QT_QPA_PLATFORM=xcb
+# AppImages | https://github.com/pkgforge-dev/Citron-AppImage/issues/50
+export URUNTIME_PRELOAD=1 QT_QPA_PLATFORM=xcb
