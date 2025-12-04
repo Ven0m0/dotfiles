67f964e11b87a5cd33f81f2ae93028b59c70037e
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-04 15:30:20 +0100

    Create zimrc.zsh

---
+++ Home/.config/zsh/zimrc.zsh
@@ -0,0 +1,45 @@
+# ============================================================================
+# Zimfw Configuration - Optimized Plugin Loading
+# ============================================================================
+
+# ---[ Core Modules ]---
+zmodule environment
+zmodule input
+zmodule utility
+
+# ---[ Completion System ]---
+zmodule zsh-users/zsh-completions --fpath src
+zmodule completion
+
+# ---[ Command History ]---
+zmodule history
+
+# ---[ Prompt ]---
+zmodule romkatv/powerlevel10k --use degit
+
+# ---[ Syntax & Suggestions ]---
+zmodule zdharma-continuum/fast-syntax-highlighting
+zmodule zsh-users/zsh-autosuggestions
+
+# ---[ Auto-pairing ]---
+zmodule hlissner/zsh-autopair
+
+# ---[ FZF Integration ]---
+zmodule lincheney/fzf-tab-completion --source zsh/fzf-zsh-completion.sh
+
+# ---[ Utility Plugins ]---
+zmodule MichaelAquilina/zsh-you-should-use
+zmodule QuarticCat/zsh-smartcache
+
+# ---[ OMZ Plugins (selective) ]---
+zmodule ohmyzsh/ohmyzsh --root plugins/git
+zmodule ohmyzsh/ohmyzsh --root plugins/sudo
+zmodule ohmyzsh/ohmyzsh --root plugins/archlinux
+zmodule ohmyzsh/ohmyzsh --root plugins/debian
+zmodule ohmyzsh/ohmyzsh --root plugins/docker
+zmodule ohmyzsh/ohmyzsh --root plugins/docker-compose
+zmodule ohmyzsh/ohmyzsh --root plugins/ssh-agent
+zmodule ohmyzsh/ohmyzsh --root plugins/command-not-found
+
+# ---[ History Navigation ]---
+zmodule zsh-users/zsh-history-substring-search
