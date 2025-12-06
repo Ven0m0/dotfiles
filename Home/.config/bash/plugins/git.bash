369b0f5be49f534cbd677233406494da26bb8951
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-06 14:43:43 +0100

    Update git.bash

--- Home/.config/bash/plugins/git.bash
+++ Home/.config/bash/plugins/git.bash
@@ -37,3 +37,11 @@ alias gh-pr-view="gh pr view --json number,title,state,url,author,createdAt,upda
 alias gh-pr-stats="gh pr view --json additions,deletions,changedFiles,files,title,state,url"
 # PR stats with full detailed information
 alias gh-pr-stats-full="gh pr view --json additions,deletions,changedFiles,files,title,author,state,createdAt,updatedAt,url,assignees,body,closed,closedAt,comments,commits,headRefName,headRefOid,isDraft,labels,mergeStateStatus,mergeable,mergedAt,mergedBy,reviewDecision,reviews"
+
+gcommits() {
+  if [ -z "$1" ]; then
+    git log --format="%C(auto)%h (%s, %ad)" -n 20 | cat
+  else
+    git log --format="%H" -n $1 | cat
+  fi
+}
