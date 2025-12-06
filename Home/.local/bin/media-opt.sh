5600c0cc04ebb98589ab1c9544022aa6a7849590
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-06 05:45:31 +0100

    Update media-opt.sh

--- Home/.local/bin/media-opt.sh
+++ Home/.local/bin/media-opt.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
-shopt -s nullglob globstar
-IFS=$'\n\t'
-export LC_ALL=C LANG=C HOME="/home/${SUDO_USER:-$USER}"
+set -euo pipefail; shopt -s nullglob globstar; IFS=$'\n\t'; export LC_ALL=C LANG=C HOME="/home/${SUDO_USER:-$USER}"
 # ==============================================================================
 # CONFIGURATION
 # ==============================================================================
