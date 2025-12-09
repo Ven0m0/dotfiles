57751c51967aef5c677e6ea5e60809f63b81b70f
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-09 07:35:58 +0100

    Update fmt-all.sh

--- Home/.local/bin/fmt-all.sh
+++ Home/.local/bin/fmt-all.sh
@@ -7,6 +7,7 @@ has tombi && tombi format
 has yamlfmt && yamlfmt -continue_on_error "*.yaml"
 has shellharden && { shellharden --replace ./*.sh||:; shellharden --replace ./*.bash||:; shellharden --replace ./*.zsh||:;}
 has biome && biome check --fix --unsafe --skip-parse-errors --no-errors-on-unmatched --html-formatter-line-width=120 --css-formatter-line-width=120 --json-formatter-line-width=120 --use-editorconfig=true --indent-style=space --format-with-errors=true --files-ignore-unknown=true --vcs-use-ignore-file=false .
+has ruff && ruff format --line-length 120
 has gh && { gh tidy; gh poi;}
 git gc --aggressive --prune=now; git repack -ab; git maintenance run
 git add -A; git commit -m "Format & Lint" && git push
