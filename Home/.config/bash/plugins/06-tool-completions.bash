916a8e7753fc98c9a7b8e09959adc3dd151ec020
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-09 13:09:26 +0100

    Update 06-tool-completions.bash

--- Home/.config/bash/plugins/06-tool-completions.bash
+++ Home/.config/bash/plugins/06-tool-completions.bash
@@ -1,14 +1,5 @@
+# ~/.config/bash/plugins/06-tool-completions.bash
 #!/usr/bin/env bash
 #=========================== [Tool Completions] ===============================
-
-# --- Cheat
-has cheat && has fzf && export CHEAT_USE_FZF=true
-
 # --- fclones
 has fclones && eval "$(fclones complete bash 2>/dev/null)" &>/dev/null
-
-# --- vx
-if has vx; then
-  eval "$(vx shell completions bash --use-system-path 2>/dev/null)" || :
-  eval "$(vx shell init bash --use-system-path 2>/dev/null)" || :
-fi
