d0c45d9d6ef65bdd41f5e2487946ee096cd27fcf
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-09 01:15:22 +0100

    Update fmt-all.sh

--- Home/.local/bin/fmt-all.sh
+++ Home/.local/bin/fmt-all.sh
@@ -1,39 +1,12 @@
 #!/usr/bin/env bash
-set -euo pipefail; shopt -s nullglob globstar
-IFS=$'\n\t' LC_ALL=C LANG=C
-
-has(){ command -v -- "$1" &>/dev/null; }
-
-if has image-optimizer; then
-  image-optimizer -r --png-optimization-level max --zopfli-iterations 100 -i .
-fi
-if has mdfmt; then
-  mdfmt . --width 120 -w
-fi
-if has tombi; then
-  tombi format
-fi
-if has yamlfmt; then
-  yamlfmt -continue_on_error "*.yaml"
-fi
-if has shellharden; then
-  # TODO: which one?
-  shellharden --replace *.sh || :
-  shellharden --replace *.bash || :
-  shellharden --replace *.zsh || :
-  #shellharden --replace **/*.sh || :
-  #shellharden --replace **/*.bash || :
-  #shellharden --replace **/*.zsh || :
-fi
-if has biome; then
-  biome check --fix --unsafe --skip-parse-errors --no-errors-on-unmatched --html-formatter-line-width=120 --css-formatter-line-width=120 \
-    --json-formatter-line-width=120 --use-editorconfig=true --indent-style=space --format-with-errors=true --files-ignore-unknown=true \
-    --vcs-use-ignore-file=false .
-fi
-if has gh; then
-   gh tidy; gh poi
-fi
-git gc --aggressive --prune=now
-git repack -ab
-git maintenance run
+set -euo pipefail;shopt -s nullglob globstar;IFS=$'\n\t';LC_ALL=C;LANG=C
+has(){ command -v -- "$1" &>/dev/null;}
+has image-optimizer && image-optimizer -r --png-optimization-level max --zopfli-iterations 100 -i .
+has mdfmt && mdfmt . --width 120 -w
+has tombi && tombi format
+has yamlfmt && yamlfmt -continue_on_error "*.yaml"
+has shellharden && { shellharden --replace ./*.sh||:; shellharden --replace ./*.bash||:; shellharden --replace ./*.zsh||:;}
+has biome && biome check --fix --unsafe --skip-parse-errors --no-errors-on-unmatched --html-formatter-line-width=120 --css-formatter-line-width=120 --json-formatter-line-width=120 --use-editorconfig=true --indent-style=space --format-with-errors=true --files-ignore-unknown=true --vcs-use-ignore-file=false .
+has gh && { gh tidy; gh poi;}
+git gc --aggressive --prune=now; git repack -ab; git maintenance run
 git add -A; git commit -m "Format & Lint" && git push
