117a92ff45581242c5914d90ea35a4b7a330011d
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-10 20:22:29 +0100

    Update fmt-all.sh

--- Home/.local/bin/fmt-all.sh
+++ Home/.local/bin/fmt-all.sh
@@ -14,8 +14,10 @@ has shellharden && { shellharden --replace ./*.sh || :; shellharden --replace ./
 has biome && biome check --fix --unsafe --skip-parse-errors --no-errors-on-unmatched --html-formatter-line-width=120 --css-formatter-line-width=120 --json-formatter-line-width=120 --use-editorconfig=true --indent-style=space --format-with-errors=true --files-ignore-unknown=true --vcs-use-ignore-file=false .
 if has uv; then 
   has ruff && ruff format --line-length 120 --target-version py311 "${PWD:-.}"
-  has black && uv tool run black -l 120 -t py313 "${PWD:-.}" 
 fi
 has gh && { gh tidy; gh poi; }
 git maintenance run --quiet --task=prefetch --task=gc --task=loose-objects --task=incremental-repack --task=pack-refs --task=reflog-expire --task=rerere-gc --task=worktree-prune &>/dev/null || :
+git submodule init
+git submodule update
+
 git add -A && git commit -q -m "Format & Lint" &>/dev/null && git push --recurse-submodules=on-demand --prune
