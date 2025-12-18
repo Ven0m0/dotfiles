9e2c8ca936091adca21915d576b8b7a2060abeeb
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-18 02:53:13 +0100

    Delete Home/.config/bash/plugins/06-tool-completions.bash

--- Home/.config/bash/plugins/06-tool-completions.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# ~/.config/bash/plugins/06-tool-completions.bash
-#=========================== [Tool Completions] ===============================
-# --- fclones
-has fclones && eval "$(fclones complete bash 2>/dev/null)" &>/dev/null
