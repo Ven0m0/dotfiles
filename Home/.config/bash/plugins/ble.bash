d1b06ae12cedee1cf1eb4e8d570f1ff888dd250e
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-18 02:54:08 +0100

    Update ble.bash

--- Home/.config/bash/plugins/ble.bash
+++ Home/.config/bash/plugins/ble.bash
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # github.com/akinomyoga/ble.sh
 [[ -r /usr/share/blesh/ble.sh ]] && . "/usr/share/blesh/ble.sh" --attach=none 2>/dev/null || {
   [[ -r "${XDG_DATA_HOME}/blesh/ble.sh" ]] && . "${XDG_DATA_HOME}/blesh/ble.sh" --noattach 2>/dev/null
