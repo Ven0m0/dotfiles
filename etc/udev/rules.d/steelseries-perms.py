de281252b89e18cdacd88d20057b005c7cbd8f47
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-21 02:07:12 +0100

    Update steelseries-perms.py

--- etc/udev/rules.d/steelseries-perms.py
+++ etc/udev/rules.d/steelseries-perms.py
@@ -4,6 +4,7 @@ import fcntl
 import os
 import struct
 import sys
+
 # from linux headers hidraw.h, hid.h, and ioctl.h
 _IOC_NRBITS = 8
 _IOC_TYPEBITS = 8
@@ -13,16 +14,31 @@ _IOC_TYPESHIFT = _IOC_NRSHIFT + _IOC_NRBITS
 _IOC_SIZESHIFT = _IOC_TYPESHIFT + _IOC_TYPEBITS
 _IOC_DIRSHIFT = _IOC_SIZESHIFT + _IOC_SIZEBITS
 _IOC_READ = 2
+
+
 def _IOC(dir, type, nr, size):
-    return (dir << _IOC_DIRSHIFT) | (ord(type) << _IOC_TYPESHIFT) | (nr << _IOC_NRSHIFT) | (size << _IOC_SIZESHIFT)
+    return (
+        (dir << _IOC_DIRSHIFT)
+        | (ord(type) << _IOC_TYPESHIFT)
+        | (nr << _IOC_NRSHIFT)
+        | (size << _IOC_SIZESHIFT)
+    )
+
+
 def _IOR(type, nr, size):
     return _IOC(_IOC_READ, type, nr, size)
+
+
 HID_MAX_DESCRIPTOR_SIZE = 4096
+
+
 class hidraw_report_descriptor(ctypes.Structure):
     _fields_ = [
         ("size", ctypes.c_uint),
         ("value", ctypes.c_uint8 * HID_MAX_DESCRIPTOR_SIZE),
     ]
+
+
 HIDIOCGRDESCSIZE = _IOR("H", 0x01, ctypes.sizeof(ctypes.c_int))
 HIDIOCGRDESC = _IOR("H", 0x02, ctypes.sizeof(hidraw_report_descriptor))
 hidraw = sys.argv[1]
