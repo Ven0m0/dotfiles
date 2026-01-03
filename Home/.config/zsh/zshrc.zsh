7c26ce5dd381faa2a7345ea8910db0aa07b1c445
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-04 00:21:02 +0100

    Update zshrc.zsh

--- Home/.config/zsh/zshrc.zsh
+++ Home/.config/zsh/zshrc.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 # shellcheck shell=bash
 # ============================================================================
-# Fish-Like Zsh Config - Optimized & Consolidated
+# Zsh Main Config - Fish-Like & Modular
 # ============================================================================
 [[ $- != *i* ]] && return
 
@@ -45,29 +45,19 @@ export HISTFILE="${HISTFILE:-$HOME/.zsh_history}" HISTSIZE=50000 SAVEHIST=50000
 # ---[ Path ]---
 typeset -gU path fpath
 path=(~/.local/bin ~/.cargo/bin /usr/local/{bin,sbin} /usr/{bin,sbin})
+fpath=("$ZDOTDIR/completions" "$XDG_DATA_HOME/zsh/completions" $fpath)
 
-# ---[ Completion Styles ]---
-zstyle ':completion:*' use-cache on cache-path "$XDG_CACHE_HOME/zsh/zcompcache"
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-zstyle ':completion:*' menu select list-colors ${(s.:.)LS_COLORS} group-name ''
-zstyle ':completion:*:descriptions' format '%F{yellow}-- %d --%f'
-zstyle ':completion:*:warnings' format '%F{red}-- no matches --%f'
-zstyle ':completion:*' insert-tab false squeeze-slashes true special-dirs ..
-zstyle ':completion:*:git-checkout:*' sort false
-zstyle ':fzf-tab:*' use-fzf-default-opts yes
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath 2>/dev/null || ls -1 $realpath'
-
-# ---[ Keybindings (Fish-Like) ]---
-bindkey -e  # Emacs base
-bindkey '^[[A' history-substring-search-up      # Up arrow
-bindkey '^[[B' history-substring-search-down    # Down arrow
+# ---[ Keybindings ]---
+bindkey -e
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
 bindkey '^P' history-substring-search-up
 bindkey '^N' history-substring-search-down
-bindkey '^[[Z' reverse-menu-complete            # Shift+Tab
-bindkey '^[[1;5C' forward-word                  # Ctrl+Right
-bindkey '^[[1;5D' backward-word                 # Ctrl+Left
-bindkey '^W' backward-kill-word                 # Ctrl+W
-bindkey '^H' backward-delete-char               # Ctrl+Backspace
+bindkey '^[[Z' reverse-menu-complete
+bindkey '^[[1;5C' forward-word
+bindkey '^[[1;5D' backward-word
+bindkey '^W' backward-kill-word
+bindkey '^H' backward-delete-char
 autoload -U select-word-style && select-word-style bash
 
 # ---[ Deferred Init ]---
@@ -81,152 +71,10 @@ else
   has fzf && eval "$(fzf --zsh)"
 fi
 
-# ---[ Aliases - Modern Tools ]---
-if has eza; then
-  alias ls='eza --git --icons -F --group-directories-first --time-style=long-iso'
-  alias ll='eza --git --icons -F --group-directories-first --time-style=long-iso -alh'
-  alias la='eza --git --icons -F --group-directories-first --time-style=long-iso -a'
-  alias lt='eza --git --icons -F --group-directories-first --time-style=long-iso -T'
-  alias l='eza --git --icons -F --group-directories-first --time-style=long-iso -1'
-fi
-has bat && alias cat='bat --style=plain --paging=never' bathelp='bat -plhelp'
-has rg && alias grep='rg'
-has fd && alias find='fd'
-has dust && alias du='dust'
-has sd && alias sed='sd'
-has jaq && alias jq='jaq'
-has sk && alias fzf='sk'
-
-# Navigation
-alias -g ...='../..' ....='../../..' .....='../../../..'
-alias -- -='cd -'
-
-# Git (prefer gix)
-if has gix; then
-  alias g=gix ga='gix add' gaa='gix add -A' gc='gix commit' gcm='gix commit -m'
-  alias gca='gix commit --amend' gcan='gix commit --amend --no-edit'
-  alias gp='gix push' gpf='gix push --force-with-lease' gl='gix pull'
-  alias gst='gix status' gd='gix diff' gds='gix diff --staged'
-  alias gco='gix checkout' gcb='gix checkout -b' gb='gix branch'
-  alias glog='gix log --oneline --graph --decorate'
-  alias grb='gix rebase' grbi='gix rebase -i' gsh='gix stash'
-else
-  alias g=git ga='git add' gaa='git add -A' gc='git commit' gcm='git commit -m'
-  alias gca='git commit --amend' gcan='git commit --amend --no-edit'
-  alias gp='git push' gpf='git push --force-with-lease' gl='git pull'
-  alias gst='git status' gd='git diff' gds='git diff --staged'
-  alias gco='git checkout' gcb='git checkout -b' gb='git branch'
-  alias glog='git log --oneline --graph --decorate'
-  alias grb='git rebase' grbi='git rebase -i' gsh='git stash'
-fi
-
-# Arch package manager
-if has paru; then
-  alias p=paru pi='paru -S' pu='paru -Syu' pr='paru -Rns'
-  alias ps='paru -Ss' pq='paru -Q' pc='paru -Sc'
-elif has yay; then
-  alias p=yay pi='yay -S' pu='yay -Syu' pr='yay -Rns'
-  alias ps='yay -Ss' pq='yay -Q' pc='yay -Sc'
-fi
-
-# Docker
-if has docker; then
-  alias d=docker dc='docker compose' dps='docker ps' di='docker images'
-  alias dex='docker exec -it' dlog='docker logs -f'
-fi
-
-# System
-alias c=clear h=history v='${EDITOR:-nano}' reload='exec zsh'
-alias zshrc='${EDITOR:-nano} "$ZDOTDIR/.zshrc" && reload'
-alias ffmpeg='ffmpeg -hide_banner' ffprobe='ffprobe -hide_banner'
-
-# Global aliases (zsh-specific)
-alias -g H='| head' T='| tail' G='| grep' L='| less' N='&>/dev/null'
-
-# ---[ Fish-Style Abbreviations ]---
-# These expand on space, unlike aliases
-typeset -gA abbrevs=(
-  gs  'git status'
-  gd  'git diff'
-  gc  'git commit'
-  gp  'git push'
-  gl  'git pull'
-  gco 'git checkout'
-  gaa 'git add -A'
-  gcm 'git commit -m'
-  ll  'ls -lah'
-  la  'ls -A'
-  ..  'cd ..'
-  ... 'cd ../..'
-  psg 'ps aux | grep -v grep | grep -i -e VSZ -e'
-  h   'history'
-)
-
-# Abbreviation expansion on space
-magic-abbrev-expand(){
-  local left="${LBUFFER%% *}"
-  local expanded="${abbrevs[$left]}"
-  if [[ -n $expanded ]]; then
-    LBUFFER="$expanded${LBUFFER#$left}"
-  fi
-  zle self-insert
-}
-zle -N magic-abbrev-expand
-bindkey ' ' magic-abbrev-expand
-bindkey '^ ' self-insert  # Ctrl+Space for literal space
-
-# ---[ Core Functions ]---
-mkcd(){ mkdir -p "$1" && cd "$1"; }
-up(){ local d="" i; for ((i=1; i<=${1:-1}; i++)); d+=/.. ; cd "${d#/}"; }
-extract(){
-  [[ $# -eq 0 || ! -f $1 ]] && { echo "Usage: extract <file>"; return 1; }
-  case "$1" in
-    *.tar.bz2|*.tbz2) tar xjf "$1" ;;
-    *.tar.gz|*.tgz) tar xzf "$1" ;;
-    *.tar.xz) tar xJf "$1" ;;
-    *.tar.zst) tar --zstd -xf "$1" ;;
-    *.tar) tar xf "$1" ;;
-    *.zip) unzip "$1" ;;
-    *.7z) 7z x "$1" ;;
-    *) echo "Unsupported: $1"; return 1 ;;
-  esac
-}
-
-# Fuzzy file/dir navigation
-fz(){
-  local fuzzy=$(has sk && echo sk || echo fzf)
-  local mode=dir
-  [[ $1 == -f ]] && { mode=file; shift; }
-  local path="${1:-.}"
-  if [[ $mode == file ]]; then
-    local file=$(fd -t f . "$path" 2>/dev/null | $fuzzy --preview 'bat --color=always {}')
-    [[ -n $file ]] && ${EDITOR:-nano} "$file"
-  else
-    local dir=$(fd -t d . "$path" 2>/dev/null | $fuzzy --preview 'ls -lah {}')
-    [[ -n $dir ]] && cd "$dir"
-  fi
-}
-alias fe='fz -f'
-
-# Git helpers
-gdbr(){
-  git fetch --prune
-  git branch -vv | grep ': gone]' | awk '{print $1}' | xargs -r git branch -D
-}
-
-# Package search (Arch)
-pacfzf(){
-  local fuzzy=$(has sk && echo sk || echo fzf)
-  local sel=$(paru -Ssq | $fuzzy --preview 'paru -Si {} | bat -plini --color=always')
-  [[ -n $sel ]] && paru -S "$sel"
-}
-
-# Process killer
-fkill(){
-  local fuzzy=$(has sk && echo sk || echo fzf)
-  local pid=$(ps -ef | tail -n +2 | $fuzzy -m | awk '{print $2}')
-  [[ -n $pid ]] && kill -"${1:-9}" $pid
-}
+# ---[ Load Modular Configs ]---
+ifsrc "$ZDOTDIR/config/completions.zsh"
+ifsrc "$ZDOTDIR/config/aliases.zsh"
+ifsrc "$ZDOTDIR/config/functions.zsh"
 
 # ---[ Theme ]---
 ifsrc ~/.p10k.zsh
