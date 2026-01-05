ee9029117a64c9b591c9c5caa214913fd3d5dd04
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-05 03:27:01 +0100

    Update config.fish

--- Home/.config/fish/config.fish
+++ Home/.config/fish/config.fish
@@ -5,7 +5,7 @@ if test -r /usr/share/cachyos-fish-config/cachyos-config.fish
 end
 
 function init_tool
-    if type -q $argv[1]
+    if command -qs $argv[1]
         if type -q _evalcache
             # Cache the init command output to avoid running binaries on every startup
             _evalcache "$argv[2]" >/dev/null 2>&1
@@ -21,17 +21,15 @@ set -gx COLORTERM truecolor
 set -gx FZF_DEFAULT_COMMAND 'fd -tf -H --size +1k'
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 
-fish_add_path ~/bun/bin $HOME/.local/bin/ /usr/local/bin $HOME/bin $HOME/.bin /usr/lib/ccache/bin/ $HOME/.cargo/bin/
+fish_add_path ~/bun/bin ~/.local/bin/ /usr/local/bin ~/bin /usr/lib/ccache/bin/ ~/.cargo/bin/
 
 # Tool Initializations
 init_tool fzf "fzf --fish"
 init_tool starship "starship init fish"
 init_tool zoxide "zoxide init --cmd cd fish"
 init_tool mise "mise activate fish"
-init_tool zellij "zellij setup --generate-auto-start fish"
+# init_tool zellij "zellij setup --generate-auto-start fish"
 
 # SSH agent
 set -gx SSH_AUTH_SOCK ~/.ssh/ssh-agent.sock
-if not test -e $SSH_AUTH_SOCK
-    eval (ssh-agent -c -s)
-end
+test -e $SSH_AUTH_SOCK || eval (ssh-agent -c -s)
