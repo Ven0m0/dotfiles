39938dd38b8422cb6c12f3dc4d3b85f3eaa0c5df
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-05 03:34:59 +0100

    Update batmanurl.fish

--- Home/.config/fish/functions/batmanurl.fish
+++ Home/.config/fish/functions/batmanurl.fish
@@ -6,6 +6,5 @@ function batmanurl -w man -d "view a github manpage with bat"
     # Extract raw URL from GitHub blob URL
     set url $argv[1]
     set raw_url (string replace -r 'https://github.com/([^/]+)/([^/]+)/blob/([^/]+)/(.*)' 'https://raw.githubusercontent.com/$1/$2/$3/$4' $url)
-
     curl -s "$raw_url" | bat -l man
 end
