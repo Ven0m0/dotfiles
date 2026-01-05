589198976e6baa512024451303621cd2ae87ae7e
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-05 04:12:25 +0100

    Update fenv.fish

--- Home/.config/fish/functions/fenv.fish
+++ Home/.config/fish/functions/fenv.fish
@@ -1,3 +1,3 @@
 function fenv -d ""
-    printenv | sort | column -t -s '=' | fzf --layout=reverse-list --no-mouse --no-multi --cycle --inline-info --no-scrollbar +s --height=100% --tiebreak=begin,end -e
+    printenv | sort | awk -F= '{printf "%-30s %s\n", $1, $2}' | fzf --delimiter=' ' --nth=1 --layout=reverse-list --no-mouse --no-multi --cycle --inline-info --no-scrollbar +s --height=100% --tiebreak=begin,end -e
 end
