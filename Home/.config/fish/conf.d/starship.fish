329707c6989cd4d756b975c988829e979602960f
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-05 03:29:03 +0100

    Update starship.fish

--- Home/.config/fish/conf.d/starship.fish
+++ Home/.config/fish/conf.d/starship.fish
@@ -3,9 +3,6 @@ command -qs starship || return
 function starship_transient_prompt_func
     starship module character
 end
-function starship_transient_rprompt_func
-    starship module time
-end
 if type -q _evalcache
     function my_async_prompt
         _evalcache starship prompt
