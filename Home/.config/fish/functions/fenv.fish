c5916adf6fb85c8dde165c7a1b4d49d0676488f7
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-05 06:05:14 +0100

    Update fenv.fish

--- Home/.config/fish/functions/fenv.fish
+++ Home/.config/fish/functions/fenv.fish
@@ -1,3 +1,14 @@
 function fenv -d ""
-    printenv | sort | awk -F= '{printf "%-30s %s\n", $1, $2}' | fzf --delimiter=' ' --nth=1 --layout=reverse-list --no-mouse --no-multi --cycle --inline-info --no-scrollbar +s --height=100% --tiebreak=begin,end -e
+    set -l LC_ALL=C; set -l lines; set -l max 0
+    for v in (printenv)
+        set lines $lines $v
+        set n (string split -m1 = $v)[1]
+        set l (string length -- $n)
+        test $l -gt $max && set max $l
+    end        
+    for v in $lines
+        set p (string split -m1 = $v)
+        printf "%-*s %s\n" $max $p[1] $p[2]
+    end | fzf --delimiter=' ' --nth=1 --layout=reverse-list --no-mouse --no-multi \
+            --cycle --inline-info --no-scrollbar +s --height=100% --tiebreak=begin,end -e
 end
