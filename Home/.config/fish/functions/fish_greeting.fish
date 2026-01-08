1c1e5944a5e031a61e15ebbe9931e9f71eafa298
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-08 21:40:30 +0100

    Update fish_greeting.fish

--- Home/.config/fish/functions/fish_greeting.fish
+++ Home/.config/fish/functions/fish_greeting.fish
@@ -2,6 +2,6 @@ function fish_greeting
     if command -q hyfetch
         LC_CTYPE=C LC_COLLATE=C hyfetch -m 8bit
     else if command -q fastfetch
-        LC_CTYPE=C LC_COLLATE=C fastfetch --thread true --detect-version false 
+        LC_CTYPE=C LC_COLLATE=C fastfetch --thread true --detect-version false --logo-type kitty
     end
 end
