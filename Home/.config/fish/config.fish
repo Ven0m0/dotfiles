a561c5fb1cb646467780070b84b7db46456ce585
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-08 21:04:34 +0100

    Update config.fish

--- Home/.config/fish/config.fish
+++ Home/.config/fish/config.fish
@@ -22,16 +22,31 @@ set -gx GPG_TTY (tty)
 set -gx COLORTERM truecolor
 set -gx FZF_DEFAULT_COMMAND 'fd -tf -H --size +1k'
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+set -gx EDITOR micro
+set -gx SUDO_EDITOR micro
+set -gx PAGER bat
+set -gx GIT_PAGER delta
+alias cat="bat -pp"
+alias sudo="sudo-rs "
+alias ed="$EDITOR "
+alias sued="sudo-rs $EDITOR "
 
 fish_add_path ~/bun/bin ~/.local/bin/ /usr/local/bin ~/bin /usr/lib/ccache/bin/ ~/.cargo/bin/
 
 # Tool Initializations
 init_tool fzf "fzf --fish"
-init_tool starship "starship init fish"
+init_tool starship "starship init fish" && enable_transience
 init_tool zoxide "zoxide init --cmd cd fish"
 init_tool mise "mise activate fish"
 # init_tool zellij "zellij setup --generate-auto-start fish"
+set -gx LS_COLORS "$(vivid generate molokai)"
 
 # SSH agent
 set -gx SSH_AUTH_SOCK ~/.ssh/ssh-agent.sock
 test -e $SSH_AUTH_SOCK || eval (ssh-agent -c -s)
+
+alias ls='eza -al --color --icons --group-directories-first --smart-group'
+alias la='eza -a --color --icons --group-directories-first --smart-group'
+alias ll='eza -l --color --icons --group-directories-first --smart-group'
+alias lt='eza -aT --color --icons --group-directories-first --smart-group'
+alias l.="eza -a | rg -. --color=auto -e '^\.'"
