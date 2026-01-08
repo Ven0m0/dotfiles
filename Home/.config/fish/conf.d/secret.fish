094cbb5d8f24f9b11d9f05ff75995357226538af
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-08 23:43:09 +0100

    Delete Home/.config/fish/conf.d/secret.fish

--- Home/.config/fish/conf.d/secret.fish
+++
@@ -1,6 +0,0 @@
-status -i >/dev/null 2>&1 || return
-# you see nothing...
-set -gx SHELL_MOMMYS_ONLY_NEGATIVE true
-set -gx MOMMY_SWEETIE girl
-set -gx SHELL_MOMMYS_MOODS chill/thirsty/yikes
-set -gx SHELL_MOMMYS_FUCKING pet/slut/toy/whore
