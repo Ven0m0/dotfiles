8b9734078d349aa27697c268114d3ecbebc59e50
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-09 10:06:13 +0100

    Delete Home/.config/fish/functions/ls.fish

--- Home/.config/fish/functions/ls.fish
+++
@@ -1,3 +0,0 @@
-function ls --wraps='eza -G --color --group-directories-first --icons --no-time --no-user --no-permissions --git-repos-no-status' --description 'alias ls=eza -G --color --group-directories-first --icons --no-time --no-user --no-permissions --git-repos-no-status'
-    eza -G --color --group-directories-first --icons --no-time --no-user --no-permissions --git-repos-no-status $argv
-end
