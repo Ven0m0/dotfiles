874c180b6a6696079484255b10ca70b51bd19f4c
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-09 23:06:34 +0100

    Update eza.bash

--- Home/.config/bash/plugins/eza.bash
+++ Home/.config/bash/plugins/eza.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 has eza || return
 # Eza aliases for modern ls replacement
-alias ls='eza -F --color=auto --group-directories-first --icons=auto'
-alias la='eza -aF --color=auto --group-directories-first --icons=auto'
-alias ll='eza -alF --color=auto --git --header --group-directories-first --icons=auto'
-alias lt='eza -aT -L 2 --color=auto --group-directories-first --icons=auto'
-alias tree='eza -T --color=always --icons=auto'
+alias ls='eza -F --color=auto --group-directories-first --icons=auto --no-quotes'
+alias la='eza -aF --color=auto --group-directories-first --icons=auto --no-quotes'
+alias ll='eza -alF --color=auto --git --header --group-directories-first --icons=auto --no-quotes'
+alias lt='eza -aT -L 2 --color=auto --group-directories-first --icons=auto --no-quotes'
+alias tree='eza -T --color=always --icons=auto --no-quotes'
