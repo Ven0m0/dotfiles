a9913e007c62e726b2eb7d2087ff56ffdb0477f6
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-09 10:04:43 +0100

    Delete Home/.config/fish/functions/y.fish

--- Home/.config/fish/functions/y.fish
+++
@@ -1,9 +0,0 @@
-function y -d "yazi with auto-cd"
-    command -q yazi; or return
-    set -l tmp (mktemp -t "yazi-cwd.XXXXXX")
-    yazi $argv --cwd-file=$tmp
-    if read -z cwd <$tmp; and test -n "$cwd" -a "$cwd" != "$PWD"
-        builtin cd $cwd
-    end
-    rm -f $tmp
-end
