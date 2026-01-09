81347f58484aabf0fc86bb155584cb5893a3b58a
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-09 10:05:57 +0100

    Delete Home/.config/fish/functions/suedit.fish

--- Home/.config/fish/functions/suedit.fish
+++
@@ -1,4 +0,0 @@
-function suedit -d 'Edit a file as root using $EDITOR'
-    set -q EDITOR; or set EDITOR nano
-    type -q sudo-rs; and sudo-rs $EDITOR $argv; or type -q doas; and doas $EDITOR $argv; or sudo $EDITOR $argv
-end
