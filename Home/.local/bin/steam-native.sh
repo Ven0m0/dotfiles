4ad8aadca64c0536693c666b0f158a9783be19bf
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-14 03:48:12 +0100

    Update steam-native.sh

--- Home/.local/bin/steam-native.sh
+++ Home/.local/bin/steam-native.sh
@@ -5,4 +5,4 @@ export STEAM_RUNTIME=0 STEAM_RUNTIME_HEAVY=0 DBUS_FATAL_WARNINGS=0 PULSE_LATENCY
 # Override some libraries as these are what games linked against.
 export LD_LIBRARY_PATH="/usr/lib/steam:/usr/lib32/steam${LD_LIBRARY_PATH:+:}$LD_LIBRARY_PATH"
 # -compat-force-slr off disables the forced Scount runtime on all games
-exec /usr/lib/steam/steam -no-cef-sandbox -compat-force-slr off "$@"
+exec /usr/lib/steam/steam -no-cef-sandbox -compat-force-slr off -enable-features=WaylandWindowDecorations -window-system=wayland "$@"
