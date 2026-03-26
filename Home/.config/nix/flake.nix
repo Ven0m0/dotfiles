aa12fe8d1c8d1c675292a6df7220bccca3ef6111
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-03-26 01:33:19 +0100

    Delete Home/.config/nix directory

--- Home/.config/nix/flake.nix
+++
@@ -1,8 +0,0 @@
-{
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-  };
-}
